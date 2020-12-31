a3c1bf843862c52f281c1f4d0eb8b0b23171279f
soeherman <gatotkoco419@gmail.com>
2020-12-31 15:12:34 +0700

    a
    a

--- resources/views/vendor/include/daftarkiriman.blade.php
+++ resources/views/vendor/include/daftarkiriman.blade.php
@@ -11,22 +11,46 @@
             <th>Tanggal Buat</th>
             <th>Tanggal Kirim</th>
             <th>Tujuan</th>
+            <th>Gudang</th>
+            <th>Pos</th>
             <th>Status</th>
         </tr>
     </thead>
     <tbody>
-        @php
-            $no = 1;
-        @endphp
         @foreach ($data as $d)
         <tr>
-            <td>{{ $no++ }}</td>
+            @if ($d->statuskiriman == "Mengatur")
+                <td><i class="fa fa-gears text-info"></i></td>
+            @elseif($d->statuskiriman == "Meminta")
+                <td><i class="fa fa-send text-muted"></i></td>
+            @elseif($d->statuskiriman == "Ditolak")
+                <td><i class="fa fa-close text-danger"></i></td>
+            @elseif($d->statuskiriman == "Diterima")
+                <td><i class="fa fa-check text-success"></i></td>
+            @else
+                <td></td>
+            @endif
             <td><a href="{{ url('kirimbarang/'.$d->kodekirim) }}">{{ $d->nopo }}</a></td>
             <td>{{ $d->keperluan }}</td>
             <td>{{ $d->tglbuat }}</td>
             <td>{{ $d->tglkirim }}</td>
             <td>Mbak Rini</td>
-            <td><span class="label label-success">{{ $d->statuskiriman }}</span></td>
+            <td>{{ $d->statusgudang }}</td>
+            <td>{{ $d->statuspos }}</td>
+            <td>
+                @if ($d->statusgudang == "Baru")
+                    <span class="label label-info">{{ $d->statuskiriman }}</span>
+                @elseif($d->statusgudang == "Meminta")
+                    <span class="label label-default">{{ $d->statuskiriman }}</span>
+                @elseif($d->statusgudang == "Ditolak")
+                    <span class="label label-danger">{{ $d->statuskiriman }}</span>
+                @elseif($d->statusgudang == "Diterima")
+                    <span class="label label-success">{{ $d->statuskiriman }}</span>
+                @else
+                    <td></td>
+                @endif
+               
+            </td>
         </tr>
         @endforeach
     </tbody>
